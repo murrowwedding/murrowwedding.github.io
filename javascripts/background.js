40b376403e395f5832f4e713d9e9c8a8fb964d44
tnthompson <tthompson.locsci@gmail.com>
2015-10-13 23:37:11 -0500

    Setting up the page

---
+++ javascripts/background.js
@@ -0,0 +1,17 @@
+var pic1 = '../images/photo1.jpg';
+var pic2 = '../images/photo2.jpg';
+var pic3 = '../images/photo3.jpg';
+var backgrounds = [{image: pic1}, {image: pic2}, {image: pic3}];
+var bgNumber = getRandomBg();
+
+$(document).ready(function() {
+
+  if($('body').hasClass('index'))
+  {
+    $('body').css('background-image', 'url('+backgrounds[bgNumber].image+')');
+  }
+  else
+  {
+    $('body').css('background-image', 'linear-gradient(rgba(51, 51, 51, 0.8), rgba(51, 51, 51, 0.8)), url('+backgrounds[bgNumber].image+')');
+  }
+});
