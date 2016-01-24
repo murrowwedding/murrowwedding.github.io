4e0e6178b0f8e64d9877193fa910fb7f7163b67d
tnthompson <tthompson.locsci@gmail.com>
2016-01-23 22:15:27 -0600

    Got the map to work!

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,17 @@
-console.log('This would be the main JS file.');
+$(document).ready(function() {
+  var map;
+  initMap();
+
+  var a = $(".navbar").offset().top;
+
+  $(document).scroll(function(){
+
+      if($(this).scrollTop() > a)
+      {
+         $('.navbar').css('background-color', 'rgba(51,51,51,1.0)');
+      } else {
+         $('.navbar').css({"background":"transparent"});
+      }
+  });
+
+});
